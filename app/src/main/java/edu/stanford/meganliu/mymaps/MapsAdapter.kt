e6a122501e5788de1c405d4af7817165c10747bc
Megan Liu <meganliu@stanford.edu>
2021-10-27 02:00:27 -0700

    Display a list of map titles

---
+++ app/src/main/java/edu/stanford/meganliu/mymaps/MapsAdapter.kt
@@ -0,0 +1,26 @@
+package edu.stanford.meganliu.mymaps
+
+import androidx.recyclerview.widget.RecyclerView
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import edu.stanford.meganliu.mymaps.models.UserMap
+
+class MapsAdapter(val context: Context, val userMaps: List<UserMap>): RecyclerView.Adapter<MapsAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = userMaps.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val userMap = userMaps[position]
+        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
+        textViewTitle.text = userMap.title
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+}
---
+++ app/src/main/java/edu/stanford/meganliu/mymaps/models/Place.kt
@@ -0,0 +1,4 @@
+package edu.stanford.meganliu.mymaps.models
+
+class Place {
+}
